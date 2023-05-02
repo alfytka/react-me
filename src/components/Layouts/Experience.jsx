c54f312f2ddc7ed06276280d9b4bfe17469b6871
alfytka <lyzexx04@gmail.com>
2023-05-02 21:39:14 +0700

    new background image

--- src/components/Layouts/Experience.jsx
+++ src/components/Layouts/Experience.jsx
@@ -4,8 +4,20 @@ import lksLogo from  '/images/lks-logo.png';
 const Experience = () => {
   return (
     <div className="max-w-full bg-gradient-to-b from-transparent to-cyan-50">
-      <div className="max-w-5xl mx-auto pt-10 pb-20 sm:py-20">
-        <div className="flex flex-col px-4">
+      <div className="max-w-5xl mx-auto pt-10 pb-20 sm:py-20 relative">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24 blur-sm text-red-500/40 sm:text-red-500/60 absolute top-0 right-0">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24 blur-sm text-blue-500/40 sm:text-blue-500/60 absolute top-16 right-16">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24 blur-sm text-cyan-600/40 sm:text-cyan-600/60 absolute top-0 right-32">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z" />
+        </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-24 h-24 blur-sm text-emerald-500/40 sm:text-emerald-500/60 absolute top-[7.75rem] right-0">
+          <path strokeLinecap="round" strokeLinejoin="round" d="M14.25 6.087c0-.355.186-.676.401-.959.221-.29.349-.634.349-1.003 0-1.036-1.007-1.875-2.25-1.875s-2.25.84-2.25 1.875c0 .369.128.713.349 1.003.215.283.401.604.401.959v0a.64.64 0 01-.657.643 48.39 48.39 0 01-4.163-.3c.186 1.613.293 3.25.315 4.907a.656.656 0 01-.658.663v0c-.355 0-.676-.186-.959-.401a1.647 1.647 0 00-1.003-.349c-1.036 0-1.875 1.007-1.875 2.25s.84 2.25 1.875 2.25c.369 0 .713-.128 1.003-.349.283-.215.604-.401.959-.401v0c.31 0 .555.26.532.57a48.039 48.039 0 01-.642 5.056c1.518.19 3.058.309 4.616.354a.64.64 0 00.657-.643v0c0-.355-.186-.676-.401-.959a1.647 1.647 0 01-.349-1.003c0-1.035 1.008-1.875 2.25-1.875 1.243 0 2.25.84 2.25 1.875 0 .369-.128.713-.349 1.003-.215.283-.4.604-.4.959v0c0 .333.277.599.61.58a48.1 48.1 0 005.427-.63 48.05 48.05 0 00.582-4.717.532.532 0 00-.533-.57v0c-.355 0-.676.186-.959.401-.29.221-.634.349-1.003.349-1.035 0-1.875-1.007-1.875-2.25s.84-2.25 1.875-2.25c.37 0 .713.128 1.003.349.283.215.604.401.96.401v0a.656.656 0 00.658-.663 48.422 48.422 0 00-.37-5.36c-1.886.342-3.81.574-5.766.689a.578.578 0 01-.61-.58v0z" />
+        </svg>
+        <div className="flex flex-col px-5 sm:px-4">
           <div className="bg-cyan-100/40 w-10 h-10 flex items-center justify-center rounded-md border border-cyan-200/30">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-cyan-600">
               <path d="M11.644 1.59a.75.75 0 01.712 0l9.75 5.25a.75.75 0 010 1.32l-9.75 5.25a.75.75 0 01-.712 0l-9.75-5.25a.75.75 0 010-1.32l9.75-5.25z" />
@@ -19,42 +31,7 @@ const Experience = () => {
             Pengalaman yang saya dapatkan selama ini yaitu mulai dari Praktek Kerja Lapangan disalah satu perusahaan software house dan keikutsertaan dalam mengikuti ajang perlombaan LKS SMK se-Jawa Barat Tahun 2022.
           </p>
         </div>
-        <div className="max-w-5xl mt-10 sm:mt-14 max-sm:ml-2 sm:mx-4 px-5 sm:px-10 md:px-14 py-12 bg-gradient-to-r from-transparent to-pink-100 sm:rounded-tr-3xl border-l-[3px] sm:border-l-2 border-pink-300">
-          <div className="flex justify-between items-center">
-            <div className="logos flex gap-x-5">
-              <img className="w-20 h-min" src={accessMediaLogo} alt="accessMediaLogo" />
-              <div className="title flex flex-col font-figtree">
-                <span className="text-2xl font-bold tracking-tight leading-6 text-slate-700">Access Media</span>
-                <span className="text-lg tracking-tight text-slate-500">accessmedia.co.id</span>
-              </div>
-            </div>
-            <div className="max-md:hidden bg-pink-50/20 px-3 py-1.5 rounded-lg border border-pink-200/50">
-              <h2 className="font-figtree text-base font-medium tracking-tight text-slate-700">
-                Internship for 3 months
-              </h2>
-            </div>
-          </div>
-          <div className="pt-8 text-lg font-figtree tracking-tight text-slate-700">
-            <p className="font-semibold">
-              Praktik Kerja Lapangan (7 feb - 14 Apr 2022)
-            </p>
-            <p className="max-w-2xl py-4">
-              Turut membantu perusahaan dalam mengelola data client, menuliskan tata cara penggunaan aplikasi yang dikembangkan untuk kemudahan penggunaan bagi client, dan membuat video tutorial yang diunggah pada channel YouTube Access Media.
-            </p>
-            <p className="font-semibold">
-              Responsibilities:
-            </p>
-            <div className="flex items-center gap-x-2 pt-3">
-              <span className="text-base font-medium px-4 py-1 bg-pink-50/30 text-slate-500 rounded-full border border-slate-200/50">
-                Content Developer
-              </span>
-              <span className="text-base font-medium px-4 py-1 bg-pink-50/30 text-slate-500 rounded-full border border-slate-200/50">
-                Desktop Developer
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="max-w-5xl mt-16 sm:mx-4 px-5 max-sm:ml-2 sm:px-10 md:px-14 py-12 bg-gradient-to-r from-transparent to-indigo-100 sm:rounded-tr-3xl border-l-[3px] sm:border-l-2 border-indigo-300">
+        <div className="max-w-5xl mt-10 sm:mx-4 px-5 max-sm:ml-4 sm:px-10 md:px-14 py-12 bg-gradient-to-r from-transparent to-indigo-100 sm:rounded-tr-3xl border-l-[3px] sm:border-l-2 border-indigo-300">
           <div className="flex justify-between items-center">
             <div className="logos flex gap-x-5">
               <img className="w-auto h-12" src={lksLogo} alt="lksLogo" />
@@ -84,7 +61,7 @@ const Experience = () => {
             <p className="font-semibold">
               Bidang lomba: IT Software Solution for Business
             </p>
-            <div className="flex items-center gap-x-2 pt-3">
+            <div className="flex max-sm:flex-col sm:items-center max-sm:w-max max-sm:gap-y-2 gap-x-2 pt-3">
               <span className="text-base font-medium px-4 py-1 bg-indigo-100/30 text-slate-500 rounded-full border border-sky-200/50">
                 Desktop Developer
               </span>
@@ -94,6 +71,42 @@ const Experience = () => {
             </div>
           </div>
         </div>
+        <div className="max-w-5xl mt-10 sm:mt-14 max-sm:ml-4 sm:mx-4 px-5 sm:px-10 md:px-14 py-12 bg-gradient-to-r from-transparent to-emerald-100 sm:rounded-tr-3xl border-l-[3px] sm:border-l-2 border-emerald-300">
+          <div className="flex justify-between items-center">
+            <div className="logos flex gap-x-5">
+              <img className="w-20 h-min" src={accessMediaLogo} alt="accessMediaLogo" />
+              <div className="title flex flex-col font-figtree">
+                <span className="text-2xl font-bold tracking-tight leading-6 text-slate-700">Access Media</span>
+                <span className="text-lg tracking-tight text-slate-500">accessmedia.co.id</span>
+              </div>
+            </div>
+            <div className="max-md:hidden bg-emerald-50/20 px-3 py-1.5 rounded-lg border border-emerald-200/50">
+              <h2 className="font-figtree text-base font-medium tracking-tight text-slate-700">
+                Internship for 3 months
+              </h2>
+            </div>
+          </div>
+          <div className="pt-8 text-lg font-figtree tracking-tight text-slate-700">
+            <p className="font-semibold">
+              Praktik Kerja Lapangan (7 feb - 14 Apr 2022)
+            </p>
+            <p className="max-w-2xl py-4">
+              Turut membantu perusahaan dalam mengelola data client, menuliskan tata cara penggunaan aplikasi yang dikembangkan untuk kemudahan penggunaan bagi client, dan membuat video tutorial yang diunggah pada channel YouTube Access Media.
+            </p>
+            <p className="font-semibold">
+              Responsibilities:
+            </p>
+            <div className="flex max-sm:flex-col sm:items-center max-sm:w-max max-sm:gap-y-2 gap-x-2 pt-3">
+              <span className="text-base font-medium px-4 py-1 bg-emerald-100/30 text-slate-500 rounded-full border border-slate-200/50">
+                Content Developer
+              </span>
+              <span className="text-base font-medium px-4 py-1 bg-emerald-100/30 text-slate-500 rounded-full border border-slate-200/50">
+                Desktop Developer
+              </span>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
   );
