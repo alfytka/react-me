cd68e1f5a022df5497bae31204ce328a25ae7b6d
alfytka <lyzexx04@gmail.com>
2023-05-02 21:38:25 +0700

    change icons

--- src/components/Layouts/Introduce.jsx
+++ src/components/Layouts/Introduce.jsx
@@ -10,8 +10,8 @@ const Introduce = () => {
       <div className="max-w-5xl mx-auto pt-32 pb-20">
         <div className="flex items-center gap-x-4 max-lg:px-5">
           <div className="bg-cyan-100/40 w-9 h-9 flex items-center justify-center rounded-md border border-cyan-200/30">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-cyan-600">
-              <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5l-3.9 19.5m-2.1-19.5l-3.9 19.5" />
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 text-cyan-600">
+              <path fillRule="evenodd" d="M4.804 21.644A6.707 6.707 0 006 21.75a6.721 6.721 0 003.583-1.029c.774.182 1.584.279 2.417.279 5.322 0 9.75-3.97 9.75-9 0-5.03-4.428-9-9.75-9s-9.75 3.97-9.75 9c0 2.409 1.025 4.587 2.674 6.192.232.226.277.428.254.543a3.73 3.73 0 01-.814 1.686.75.75 0 00.44 1.223zM8.25 10.875a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25zM10.875 12a1.125 1.125 0 112.25 0 1.125 1.125 0 01-2.25 0zm4.875-1.125a1.125 1.125 0 100 2.25 1.125 1.125 0 000-2.25z" clipRule="evenodd" />
             </svg>
           </div>
           <h1 className="text-3xl sm:text-4xl font-figtree tracking-tighter font-bold text-cyan-600">Introduce Myself</h1>
