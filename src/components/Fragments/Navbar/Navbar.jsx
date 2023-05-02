bb94feef1978fa2eac1ab3c1c485aef4763be6a2
alfytka <lyzexx04@gmail.com>
2023-05-02 21:40:00 +0700

    change position to relative

--- src/components/Fragments/Navbar/Navbar.jsx
+++ src/components/Fragments/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 const Navbar = (props) => {
   const { hNavClass = 'text-lg sm:text-xl font-semibold font-figtree tracking-tight text-cyan-500' } = props;
   return (
-    <div className="mt-12 sm:mt-20 px-6 sm:px-8 py-5 max-w-5xl flex justify-between mx-auto bg-gradient-to-r from-cyan-50 to-white">
+    <div className="relative z-10 mt-12 sm:mt-[5.5rem] px-6 sm:px-8 py-5 max-w-5xl flex justify-between mx-auto bg-gradient-to-r from-cyan-50 to-white">
       <h5 className={hNavClass}>Portfolio-me</h5>
       <h5 className={hNavClass}>Alfitka Haerul Kurniawan</h5>
     </div>
