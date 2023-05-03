17c3960cd72fbc698f694b05732f857e1cef0f63
alfytka <lyzexx04@gmail.com>
2023-05-03 20:29:15 +0700

    renew jumbotron

--- src/components/Layouts/Jumbotron.jsx
+++ src/components/Layouts/Jumbotron.jsx
@@ -8,13 +8,13 @@ const Jumbotron = () => {
       <Blockquote />
       <div className="isolate">
         <div className="absolute inset-0 -z-10 overflow-hidden">
-          <svg className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-cyan-300/40 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]" aria-hidden="true">
+          <svg className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-300/40 [mask-image:radial-gradient(40rem 40rem at top,white,transparent)]" aria-hidden="true">
             <defs>
-              <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="200" height="200" x="50%" y="-1" patternUnits="userSpaceOnUse">
-                <path d="M100 200V.5M.5 .5H200" fill="none" />
+              <pattern id="e813992c-7d03-4cc4-a2bd-151760b470a0" width="50" height="50" x="50%" y="-1" patternUnits="userSpaceOnUse">
+                <path d="M50 100V.5M.5 .5H100" fill="none" />
               </pattern>
             </defs>
-            <svg x="50%" y="-1" className="overflow-visible fill-cyan-50/20">
+            <svg x="50%" y="-1" className="overflow-visible fill-transparent">
               <path d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z" stroke-width="0" />
             </svg>
             <rect width="100%" height="100%" stroke-width="0" fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" />
@@ -22,7 +22,7 @@ const Jumbotron = () => {
         </div>
       </div>
       <div className="relative z-10 max-w-5xl mx-auto sm:flex justify-center px-5 py-10 sm:p-10 mt-10 sm:divide-x-2 divide-cyan-500 font-figtree gap-x-4 text-lg tracking-tight leading-6 font-normal">
-        <a href="#" className="underline decoration-wavy decoration-cyan-200 hover:decoration-cyan-400 text-cyan-500 max-sm:border-l-2 max-sm:pl-2 border-cyan-500">
+        <a href="#" className="underline decoration-wavy decoration-red-200 hover:decoration-red-400 text-cyan-500 max-sm:border-l-2 max-sm:pl-2 border-cyan-500">
           Download My CV
         </a>
         <p className="flex sm:pl-3 max-sm:pt-1 text-gray-400">
